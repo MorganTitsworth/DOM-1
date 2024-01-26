32928e1e1f84ea9ee6a29736b2549f654e2575de
AndrewArcherERA <a.archer@erasolutions.us>
2024-01-26 12:45:55 -0500

    asdf

--- project-2/js/script.js
+++ project-2/js/script.js
@@ -1,8 +1,11 @@
 const body = document.getElementsByClassName('container')
 const cards = document.querySelectorAll('section.card')
 const meme = document.getElementById('memeContainer');
-const closeButton = document.getElementById('x')
-
+const closeButton = document.getElementById('x');
+const activity = document.getElementById('activity');
+const activityContainer = document.getElementById("activityContainer")
+const closeTxt = document.getElementById("closeTxt");
+const newBtn = document.getElementById("newBtn");
 
 
 // Dark Mode Starts
@@ -38,4 +41,43 @@ cards[2].addEventListener('click', spinCard)
 
 function spinCard() {
     cards[2].classList.toggle('spin')
+}
+
+//Bored API
+cards[3].addEventListener('click', getActivity)
+newBtn.addEventListener("click", newActivity)
+closeTxt.addEventListener('click', closeActivity)
+
+const apiUrl = 'http://www.boredapi.com/api/activity/'
+let visible=false;
+
+function getActivity() {
+    fetch(apiUrl)
+        .then(res => {
+            if(!res.ok) {
+                throw new Error('Error: ' + res.status)
+            }
+
+            return res.json();
+        })
+        .then(data => {
+            activity.textContent = " " + data.activity;
+        })
+    
+    if(!visible) {
+        activityContainer.classList.toggle('showActivity')
+        visible = true;
+        cards[3].removeEventListener("click", getActivity)
+        console.log("card")
+    }
+
+};
+
+function newActivity() {
+    getActivity()
+}
+
+function closeActivity() {
+    activityContainer.classList.remove('showActivity')
+    visible = false;
 }
