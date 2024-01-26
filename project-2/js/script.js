1b8324ff881db7c9c3b8bdc382b2154b65405989
AndrewArcherERA <a.archer@erasolutions.us>
2024-01-26 08:36:48 -0500

    asd

---
+++ project-2/js/script.js
@@ -0,0 +1,34 @@
+const body = document.getElementsByClassName('container')
+const cards = document.querySelectorAll('section.card')
+const meme = document.getElementById('memeContainer');
+const closeButton = document.getElementById('x')
+
+
+
+// Dark Mode Starts
+let isDark = false;
+cards[0].addEventListener('click', darkMode)
+function darkMode() {
+    if(!isDark){
+        body[0].classList.toggle('darkBody')
+        cards.forEach((item) => item.classList.toggle('darkCard'))
+        isDark = true;
+    } else {
+        body[0].classList.toggle('darkBody')
+        cards.forEach((item) => item.classList.toggle('darkCard'))
+        isDark = false;
+    }
+    console.log("clicked btn1")
+}
+
+//Meme Starts
+cards[1].addEventListener('click', showMeme)
+closeButton.addEventListener('click', hideMeme)
+
+function showMeme() {
+    meme.classList.toggle('showMeme')
+}
+
+function hideMeme() {
+    meme.classList.toggle('showMeme')
+}
